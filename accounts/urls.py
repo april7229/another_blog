5bb41317f7485c4a6680b7be87e82b45e2ad15f2
april7229 <april83074@4261811f904c>
2018-11-25 22:40:31 +0000

    christams swap

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from . import views
+from .import views
 
 
-app_name = 'accounts'
+app_name='accounts'
 
 urlpatterns = [
-    url(r'^signup/$', views.signup_view, name = "signup")
+    url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'logout/$',views.logout_view, name="logout"),
 
     ]
--- articles/models.py
+++ articles/models.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=500)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None)
 
     def __str__(self):
         return self.title
